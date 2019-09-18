e0c7bcde30076b15cc1d49bfecb58c7c8aa2e956
ThomasVanRijn <302190448@student.rocmdondriaan.nl>
2019-09-18 13:42:02 +0200

    beginnetje

--- index.php
+++ index.php
@@ -1,13 +1,44 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>TMN-web</title>
+    <title>TMN-Web</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/index.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </head>
 <body>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+    <a href="index.php" class="navbar-brand">
+        <img src="img/logo.png" alt="logo" style="width:75px;">
+    </a>
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a href="index.php" class="nav-link">Home</a>
+        </li>
+        <li class="nav-item">
+            <a href="over-ons.php" class="nav-link">Over ons</a>
+        </li>
+        <li class="nav-item">
+            <a href="contact.php" class="nav-link">Contact</a>
+        </li>
+    </ul>
+</nav>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-6">
+            <img class="img-fluid" src="img/BG1.png" alt="bg1">
+        </div>
+        <div class="col">
+
+        </div>
+    </div>
+</div>
 
 </body>
 </html>
