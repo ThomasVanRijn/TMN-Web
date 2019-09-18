04923bd488f8a0203674f287f45c5f7e60121fd9
ThomasVanRijn <302190448@student.rocmdondriaan.nl>
2019-09-18 12:19:15 +0200

    New site

--- js/over_ons.js
+++
@@ -1,8 +0,0 @@
-function navigation() {
-  var x = document.getElementById("content");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
