9caf1a5e3540e19a5c9e2fa4f9cad8bf8f2350ea
BuildTools <unconfigured@null.spigotmc.org>
2019-10-10 19:58:31 +0200

    homepage

--- index.php
+++ index.php
@@ -11,7 +11,6 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
 <body>
-
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
     <a href="index.php" class="navbar-brand">
         <img src="img/logo.png" alt="logo" style="width:75px;">
@@ -21,7 +20,7 @@
             <a href="index.php" class="nav-link">Home</a>
         </li>
         <li class="nav-item">
-            <a href="over-ons.php" class="nav-link">Over ons</a>
+            <a href="over_ons.php" class="nav-link">Over ons</a>
         </li>
         <li class="nav-item">
             <a href="contact.php" class="nav-link">Contact</a>
@@ -31,11 +30,42 @@
 
 <div class="container-fluid">
     <div class="row">
-        <div class="col-6">
-            <img class="img-fluid" src="img/BG1.png" alt="bg1">
+        <div class="col-7 image1"></div>
+        <div class="col align-self-center text">
+            <h2>Web Development</h2>
+                <p>
+                    Een website, webshop of webapplicatie voor de lange termijn? Dan heb je een goede basis nodig. Deze
+                    basis leg je door middel van goede code. Onze developers weten exact hoe iets opgebouwd moet worden en
+                    zorgen ervoor dat aanpassingen en optimalisaties ten alle tijden mogelijk is, wij programmeren alles
+                    netjes met code hierin maken wij geavanceerde websites, webshops en applicaties. Op verzoek zouden we
+                    ook met Wordpress. Wij ontwikkelen alles handmatig maar pakken onze projecten slim aan. Door onze
+                    ervaring hebben we al redelijk wat functies gebouwd. Deze functies worden gedocumenteerd in onze interne
+                    kennisbank zodat we deze functies kunnen gebruiken bij elk nieuw project. Alles wat wij bouwen is
+                    schaalbaar, ten alle tijden uit te breiden, aan te passen, te integreren en te optimaliseren.
+                </p>
+            </div>
         </div>
-        <div class="col">
-
+    <div class="row">
+        <div class="col align-self-center text">
+            <h2>Web Design</h2>
+            <p>
+                Als wij een web ontwerp voor jou gaan maken wil je natuurlijk weten hoe wij dit doen en wat je kunt
+                verwachten. Wij zorgen ervoor dat jouw visie en doelstellingen naadloos overloopt in een grafisch
+                ontwerp voor je website, webshop of webapplicatie. Wij verstaan dit onder het simpelweg ontwerpen van
+                een webspecifiek ontwerp.
+            </p>
+        </div>
+        <div class="col-7 image2"></div>
+    </div>
+    <div class="row">
+        <div class="col-7 image3"></div>
+        <div class="col align-self-center text">
+            <h2>Logo Design</h2>
+            <p>
+                Een logo is een uithangbord, het geeft in één oogopslag een indruk van jouw bedrijf. Het logo is een
+                belangrijk onderdeel van jouw visuele identiteit. Ga bijvoorbeeld maar eens na hoe vaak een logo in
+                beeld is op een website, bij de meeste websites staat het logo continu links bovenin.
+            </p>
         </div>
     </div>
 </div>
