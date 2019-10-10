5a75b0993ff693e260be0c0cb600347c2a7cf464
Mathijs01 <302974175@student.rocmondriaan.nl>
2019-10-10 21:58:34 +0200

    over_ons aangepast

--- over_ons.php
+++ over_ons.php
@@ -5,7 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/default_layout.css">
     <link rel="stylesheet" href="css/over_ons.css">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -30,13 +29,23 @@
 </nav>
 
 <header>
-<!--    <img src="img/over_onsHead.png" alt="foto">-->
+    <div class="container">
+        <div class="row">
+            <div class="col-sm text-center">
+                <h1>Jouw partner in web development</h1>
+            </div>
+        </div>
+    </div>
+
+<!--    <div class="Partner text-center">-->
+<!--        <h1>Jouw Online Partner</h1>-->
+<!--    </div>-->
 </header>
 
 <main>
     <div class="container">
         <div class="row">
-            <div class="col-sm-4">
+            <div class="col-sm">
                 <h3>Wie zijn wij</h3>
                 <p>Wij zijn een allround bedrijf die altijd klaar staat voor de klant. Opgericht met het oogpunt om projecten op te leveren die meerwaarde bieden voor de klant.
                     Wil jij als organisatie opvallen en jouw klanten een succesvolle ervaring schenken? Dan ben je toe aan maatwerk. Het begint allemaal met de basis,
@@ -44,11 +53,11 @@
                     Een web applicatie, website of webshop moet goed opgebouwd zijn om ook doorontwikkeld te worden.  Projecten die wij opleveren zijn
                     maatwerk, voor de lange termijn geschikt en beschikbaar voor alle devices.</p>
             </div>
-            <div class="col-sm-4">
+            <div class="col-sm">
                 <h3>Column 2</h3>
                 <p></p>
             </div>
-            <div class="col-sm-4">
+            <div class="col-sm">
                 <h3>Column 3</h3>
                 <p></p>
             </div>
@@ -57,7 +66,18 @@
 </main>
 
 <footer>
-
+    <div class="container">
+        <div class="row">
+            <div class="col-sm text-right">
+                <h3>Column 1</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+            </div>
+            <div class="col-sm text-left">
+                <h3>Column 2</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+            </div>
+        </div>
+    </div>
 </footer>
 
 <?php
