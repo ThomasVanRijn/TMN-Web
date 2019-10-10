a9a1be32d560cd965d15f0689fa249d3a7254656
BuildTools <unconfigured@null.spigotmc.org>
2019-10-10 21:45:05 +0200

    contactpage

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/default_layout.css">
     <link rel="stylesheet" href="css/index.css">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -69,6 +70,17 @@
         </div>
     </div>
 </div>
-
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col text-right">
+                <p>test</p>
+            </div>
+            <div class="col">
+                <p>test</p>
+            </div>
+        </div>
+    </div>
+</footer>
 </body>
 </html>
---
+++ sendmail.php
@@ -0,0 +1,14 @@
+<?php
+if (isset($_GET['verzenden'])) {
+    $naam = $_GET['naam'];
+    $email = $_GET['email'];
+    $onderwerp = $_GET['onderwerp'];
+    $bericht = $_GET['bericht'];
+
+    $mailTo = "contact@tmn-web.com";
+    $headers = "From: " . $email;
+    $txt = "Je hebt een mail ontvangen van " . $naam . ".\n\n" . $bericht;
+
+    mail($mailTo, $onderwerp, $bericht, $headers);
+    header("Location: contact.php?mailsend");
+}
