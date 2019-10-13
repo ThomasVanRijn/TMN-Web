b11b4d1f2e83992fae3f3100452d7c0c0169f7fe
ThomasVanRijn <302190448@student.rocmondriaan.nl>
2019-10-13 19:54:32 +0200

    een paar aanpassingen gedaan

--- index.php
+++ index.php
@@ -4,46 +4,29 @@
     <title>TMN-Web</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <?php require("global_components/bootstrap.html"); ?>
     <link rel="stylesheet" href="css/default_layout.css">
     <link rel="stylesheet" href="css/index.css">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
 <body>
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
-    <a href="index.php" class="navbar-brand">
-        <img src="img/logo.png" alt="logo" style="width:75px;">
-    </a>
-    <ul class="navbar-nav">
-        <li class="nav-item">
-            <a href="index.php" class="nav-link">Home</a>
-        </li>
-        <li class="nav-item">
-            <a href="over_ons.php" class="nav-link">Over ons</a>
-        </li>
-        <li class="nav-item">
-            <a href="contact.php" class="nav-link">Contact</a>
-        </li>
-    </ul>
-</nav>
+
+<?php require("global_components/navbar.html"); ?>
 
 <div class="container-fluid">
     <div class="row">
         <div class="col-7 image1"></div>
         <div class="col align-self-center text">
             <h2>Web Development</h2>
-                <p>
-                    Een website, webshop of webapplicatie voor de lange termijn? Dan heb je een goede basis nodig. Deze
-                    basis leg je door middel van goede code. Onze developers weten exact hoe iets opgebouwd moet worden en
-                    zorgen ervoor dat aanpassingen en optimalisaties ten alle tijden mogelijk is, wij programmeren alles
-                    netjes met code hierin maken wij geavanceerde websites, webshops en applicaties. Op verzoek zouden we
-                    ook met Wordpress. Wij ontwikkelen alles handmatig maar pakken onze projecten slim aan. Door onze
-                    ervaring hebben we al redelijk wat functies gebouwd. Deze functies worden gedocumenteerd in onze interne
-                    kennisbank zodat we deze functies kunnen gebruiken bij elk nieuw project. Alles wat wij bouwen is
-                    schaalbaar, ten alle tijden uit te breiden, aan te passen, te integreren en te optimaliseren.
-                </p>
+            <p>
+                Een website, webshop of webapplicatie voor de lange termijn? Dan heb je een goede basis nodig. Deze
+                basis leg je door middel van goede code. Onze developers weten exact hoe iets opgebouwd moet worden en
+                zorgen ervoor dat aanpassingen en optimalisaties ten alle tijden mogelijk is, wij programmeren alles
+                netjes met code hierin maken wij geavanceerde websites, webshops en applicaties. Op verzoek zouden we
+                ook met Wordpress. Wij ontwikkelen alles handmatig maar pakken onze projecten slim aan. Door onze
+                ervaring hebben we al redelijk wat functies gebouwd. Deze functies worden gedocumenteerd in onze interne
+                kennisbank zodat we deze functies kunnen gebruiken bij elk nieuw project. Alles wat wij bouwen is
+                schaalbaar, ten alle tijden uit te breiden, aan te passen, te integreren en te optimaliseren.
+            </p>
             </div>
         </div>
     <div class="row">
@@ -70,17 +53,6 @@
         </div>
     </div>
 </div>
-<footer>
-    <div class="container">
-        <div class="row">
-            <div class="col text-right">
-                <p>test</p>
-            </div>
-            <div class="col">
-                <p>test</p>
-            </div>
-        </div>
-    </div>
-</footer>
+<?php require("global_components/footer.html"); ?>
 </body>
 </html>
